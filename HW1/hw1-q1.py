34f0cad77c9a755c8f25ce64a5de0a594e63ef76
msrgomez <monicasrgomez@gmail.com>
2022-12-22 11:40:34 +0000

    First try on 1.2 b) - Failed

--- HW1/hw1-q1.py
+++ HW1/hw1-q1.py
@@ -87,18 +87,79 @@ class LogisticRegression(LinearModel):
 
 
 class MLP(object):
-    # Q3.2b. This MLP skeleton code allows the MLP to be used in place of the
+    # Q1.2b. This MLP skeleton code allows the MLP to be used in place of the
     # linear models with no changes to the training loop or evaluation code
     # in main().
-    def __init__(self, n_classes, n_features, hidden_size):
-        # Initialize an MLP with a single hidden layer.
-        raise NotImplementedError
+    def __init__(self, n_classes, n_features, hidden_size, n_layers):
+        # Initialize an MLP with a single hidden layer. Biases to zero, weights to gaussian
+        # with mean 0 and std 0.1.
+        self.W1 = np.random.normal(0, 0.1, (hidden_size, n_features))
+        self.b1 = np.zeros(hidden_size)
+
+        self.W2 = np.random.normal(0, 0.1, (n_classes, hidden_size))
+        self.b2 = np.zeros(n_classes)
+
+        # Agreggate all weights and biases into lists.
+        self.W = [self.W1, self.W2]
+        self.b = [self.b1, self.b2]
+
+        self.activation = [self.relu, self.softmax]
+
+        # Store the number of layers.
+        self.n_layers = n_layers + 2 # +2 for input and output layers
+
+    def relu(self, input):
+        return np.maximum(0, input)
+
+    def softmax(self, input):
+        return np.exp(input) / np.sum(np.exp(input))
+
+    def forward_pass(self, x_i):
+        # Compute the forward pass. Return values of hidden nodes and output.
+        # x_i (n_features): a single training example
+
+        # Initialize the list of hidden nodes.
+        hidden_nodes = [x_i]
+
+        # Loop over the layers.
+        for i in range(self.n_layers - 1):
+            z = np.dot(self.W[i], hidden_nodes[-1]) + self.b[i]
+            a = self.activation[i](z)
+            
+            hidden_nodes.append(a)
+        print("hidden_nodes", hidden_nodes[-1])
+        return hidden_nodes
 
     def predict(self, X):
         # Compute the forward pass of the network. At prediction time, there is
         # no need to save the values of hidden nodes, whereas this is required
         # at training time.
-        raise NotImplementedError
+        predictions = [] # y_hat (n_examples)
+
+        # Loop example by example
+        for x_i in X:
+            # Compute the forward pass.
+            hidden_nodes = self.forward_pass(x_i)
+
+            print("hidden_nodes", hidden_nodes[-1])
+            # Get the prediction.
+            predictions.append(np.argmax(hidden_nodes[-1]))
+            print("predictions", predictions[-1])
+            exit()
+
+            # Forward pass
+            # Hidden layer
+            #z1 = self.W1.dot(x_i) + self.b1 # Dot product of weights and inputs plus bias
+            #h1 = self.relu(z1) # ReLU activation for hidden layer
+
+            # Output layer
+            #z2 = self.W2.dot(h1) + self.b2 # Dot product of weights and inputs plus bias
+            #h2 = self.softmax(z2) # Softmax activation for output layer
+
+            # Get prediction
+            #predictions.append(np.argmax(h2))
+
+        return predictions
 
     def evaluate(self, X, y):
         """
@@ -107,13 +168,74 @@ class MLP(object):
         """
         # Identical to LinearModel.evaluate()
         y_hat = self.predict(X)
+        print(y_hat[0:5], y[0:5])
         n_correct = (y == y_hat).sum()
         n_possible = y.shape[0]
         return n_correct / n_possible
 
-    def train_epoch(self, X, y, learning_rate=0.001):
-        raise NotImplementedError
+    def cross_entropy_loss(self, y, y_pred):
+        # Compute the cross-entropy loss. This is the loss function used for
+        # multiclass classification.
+        """
+        y (n_examples): gold labels
+        y_pred (n_examples x n_classes): predicted outputs
+        """
 
+        # Compute predicted probabilities.
+        pred_probabilities = self.softmax(y_pred)
+
+        # Compute the cross-entropy loss.
+        loss = -np.sum(y * np.log(pred_probabilities))
+
+        return loss / float(y_pred.shape[0])
+
+    def grad_descent(self, x_i, y_i, learning_rate=0.001):
+        # Compute stochastic gradient descent backpropagation. Loss function is
+        # cross-entropy. Use the chain rule to compute the gradients of the loss
+        # with respect to the weights and biases.
+        """
+        x_i (n_features): a single training example
+        y_i: the gold label for that example
+        learning_rate (float): rate at which to update weights
+        """
+
+        # Forward pass
+        hidden_nodes = self.forward_pass(x_i)
+
+        # Cross-entropy loss
+        ce_loss = self.cross_entropy_loss(y_i, hidden_nodes[-1])
+
+        # Backward pass
+        # Output layer
+        dW2 = np.dot(hidden_nodes[-2].T, ce_loss)
+        db2 = np.sum(ce_loss, axis=0)
+
+        # Hidden layer
+        dW1 = np.dot(hidden_nodes[-3].T, ce_loss)
+        db1 = np.sum(ce_loss, axis=0)
+
+        # Update weights and biases
+        self.W1 -= learning_rate * dW1
+        self.b1 -= learning_rate * db1
+
+        self.W2 -= learning_rate * dW2
+        self.b2 -= learning_rate * db2
+
+        print("W1", self.W1)
+        print("b1", self.b1)
+        print("W2", self.W2)
+        print("b2", self.b2)
+        exit()
+        
+    def train_epoch(self, X, y, learning_rate=0.001):
+        # Train the model for one epoch.
+        # X (n_examples x n_features)
+        # y (n_examples): gold labels
+        # learning_rate (float): rate at which to update weights
+
+        # Loop example by example
+        for x_i, y_i in zip(X, y):
+            self.grad_descent(x_i, y_i, learning_rate)
 
 def plot(epochs, valid_accs, test_accs):
     plt.xlabel('Epoch')
